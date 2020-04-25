e4b1bae1e1651f91dc2d3fbf957f708591659481
Jake Browning <jake.reid.browning@gmail.com>
2020-04-25 15:33:28 +0100

    Fix word type extraction bug

--- fe-components/word-search/src/WordTypeGroup/utils.ts
+++ fe-components/word-search/src/WordTypeGroup/utils.ts
@@ -1,4 +1,7 @@
 export const specificWordType = (wordType: string): string => {
+    if (!wordType.includes("(")) {
+        return wordType;
+    }
     const wordSplit = wordType.split("(");
     const bracketsContent = wordSplit[1].replace(")", "");
     const baseType = wordSplit[0].trim();
